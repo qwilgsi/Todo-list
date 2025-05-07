babd5716465f6e16136c206f4d996762dbb297b5
qwilgsi <orafan100@gmail.com>
2025-05-07 03:10:23 +0300

    Update

--- src/pages/Todo/index.js
+++ src/pages/Todo/index.js
@@ -4,29 +4,35 @@ import { useSelector } from "react-redux";
 import { selectTodoById } from "../../store/todos/selectors";
 
 export default function Todo() {
-    const { id } = useParams();
-    const todo = useSelector((state) => selectTodoById(state, id));
+  const { id } = useParams();
+  const todo = useSelector((state) => selectTodoById(state, id));
 
-    const { title } = todo;
-
+  if (!todo) {
     return (
-        <div className="container">
-            <div className="row">
-                <Breadcrumbs />
-            </div>
-            <div className="mb-3">
-                <div className="row g-0">
-                    <div className="col-md-5">
-                        <h5>{title}</h5>
-                        <p>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting
-                            industry. Lorem Ipsum has been the industry's standard dummy text
-                            ever since the 1500s, when an unknown printer took a galley of
-                            type and scrambled it to make a type specimen book.
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
+      <div className="container">
+        <p>Задачу не знайдено</p>
+      </div>
     );
+  }
+
+  const { title } = todo;
+
+  return (
+    <div className="container">
+      <div className="row">
+        <Breadcrumbs />
+      </div>
+      <div className="mb-3">
+        <div className="row g-0">
+          <div className="col-md-5">
+            <h5>{title}</h5>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry...
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/store/todos/selectors.js
+++ src/store/todos/selectors.js
@@ -1,5 +1,6 @@
 export const selectIsLoading = (state) => state.todos.isLoading;
 export const selectIsError = (state) => state.todos.isError;
 export const selectTodos = (state) => state.todos.data;
-export const selectTodoById = (state, id) => state.todos.data.find((item) => item.id === Number(id));
+export const selectTodoById = (state, id) =>
+  state.todos.data.find((item) => String(item.id) === String(id));
 export const selectEditedElement = (state) => state.todos.EditedElement;
